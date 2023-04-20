c532f5676e355c25d8978b29812b39f2ee57d9b7
Pasparakis, George <George.Pasparakis@peoplecert.org>
2023-04-20 11:11:14 +0300

    Add project files.

---
+++ ConsoleApp2/BaseClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public abstract class BaseClass
+    {
+        public string Name { get; set; }
+
+        public List<Employee>? Employees { get; set; }
+    }
+
+
+    public class Employee : BaseClass 
+    {
+        public string Title { get; set; }
+        public string ReportingTo { get; set; }        
+    
+    }
+
+    public class SubDepartment : LevelX<T>
+    {
+        //List<Employee>? Employees { get; set; }
+    }
+
+    public class Department : LevelX<SubDepartment>
+    {
+        //public List<SubDepartment> SubDepartments { get; set; }
+        
+    }
+
+    public class Division: LevelX<Department> 
+    {
+        //List<Department> Departments { get; set; }
+    }
+
+    public enum LevelType
+    {
+        Division,
+        Department,
+        SubDepartment
+    }
+    public class LevelX<T>: BaseClass 
+    {
+        public string LevelName { get; set; }
+        public LevelType Type { get; set; }
+        public List<T>? SubLevel { get; set; }
+    }
+    
+
+    public class MainObject : BaseClass 
+    {
+        public List<Level> Levels { get; set; }
+        public List<BaseClass> Employees { get; set; }
+    
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var CEO = new MainObject()
+            {
+                Name = "BN",
+            };
+
+            var groupFinance = new MainObject()
+            {
+                Name = "Group Finance",
+                Levels = new List<BaseClass>()
+                {
+                    new MainObject()
+                    {
+                        Name = "Dept1"
+                    },
+                    new MainObject()
+                    {
+                        Name = "Dept2"
+                    },
+                    new MainObject()
+                    {
+                        Name = "Dept3"
+                    },
+                    new MainObject()
+                    {
+                        Name = "Dept4"
+                    },
+                }
+                
+            }
+        }
+    }
+}
